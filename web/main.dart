7a289d924a81de062d30e59717f9b7342de6fe85
Francisco Reyna <franciscorgs@hotmail.es>
2020-10-10 01:06:38 -0500

    Added angular router

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,13 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:bootstrap_bundle/bootstrap_bundle.dart';
 import 'package:ARShopUTC/app_component.template.dart' as ng;
 import 'package:bloc/bloc.dart';
 import 'SimpleBlocObserver.dart';
 
+@GenerateInjector(
+  routerProvidersHash,
+)
 void main() {
   print(bootstrapVersion);
   print(jQueryVersion);
